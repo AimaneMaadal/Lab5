d38ccc4beedcd8ad5afdb974ba8c8f177747023b
AimaneMaadal <aimanemaadal@gmail.com>
2022-10-05 09:52:15 +0200

    test: test 2 localstorage

--- lab3/js/weather.js
+++ lab3/js/weather.js
@@ -40,6 +40,7 @@ export default class Wheather {
             let now = new Date().getTime();
             let diff = now - time;
             let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+            console.log(minutes);
             if (minutes > 1) {
                 this.fetchWeather();
                 console.log("fetching new weather");
