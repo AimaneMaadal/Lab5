1c12429d778263350b302d9a38c769d881147681
AimaneMaadal <aimanemaadal@gmail.com>
2022-10-05 05:18:12 +0200

    fix: load localstorage with 1hour buffer

--- lab3/main.js
+++ lab3/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 // import another class
-import weather from "./js/weather.js";
+import weather from "./js/wheather.js";
 
 // kickstart the app by calling the constructor via the `new` keyword
 new weather();
