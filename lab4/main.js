c88ea7c18c732b05084533f8d38780ecfbd818e2
AimaneMaadal <aimanemaadal@gmail.com>
2022-10-11 15:39:39 +0200

    feat: load donut element

--- lab4/main.js
+++ lab4/main.js
@@ -1,23 +1,73 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+const scene = new THREE.Scene();
+let light;
+let light2;
+let Mesh = null;
+let Cup = null;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function init() {
+  scene.background = new THREE.Color('#FFF0CF');
+  camera.position.set(0, 0, 20);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+}
 
-setupCounter(document.querySelector('#counter'))
+//three js add soft light
+function setLight() {
+  //add a pointlight
+  light2 = new THREE.PointLight(0xffffff, 1, 100);
+  light2.position.set(0, 0, 0);
+  light = new THREE.AmbientLight(0xffffff, 1);
+  scene.add(light, light2);
+}
+
+function loadGLTF() {
+  let Loader = new THREE.GLTFLoader();
+  Loader.load('./model/donutv4.glb', (gltf) => {
+    //load model and put it in Mesh variable
+    Mesh = gltf.scene;
+    Mesh.scale.set(0.5, 0.5, 0.5);
+    Mesh.position.set(0, 6, 0);
+    scene.add(Mesh);
+  });
+
+  //load second model
+
+
+
+}
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  if (Mesh) {
+    Mesh.rotation.x += 0.005;
+    Mesh.rotation.y += 0.01;
+  }
+  if (Cup) {
+    Cup.rotation.y += 0.005;
+  }
+  renderer.render(scene, camera);
+}
+
+function animate2() {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+}
+
+
+init();
+setLight();
+loadGLTF();
+animate();
+animate2();
+
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
