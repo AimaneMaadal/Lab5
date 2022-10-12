3c4715231bc44c5b5daca1464ea517e2eca23830
AimaneMaadal <aimanemaadal@gmail.com>
2022-10-12 22:00:42 +0200

    feat: route to message return hello world

--- lab5/routes/index.js
+++ lab5/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My first Api by Aimane' });
+
 });
 
 module.exports = router;
--- lab5/routes/message.js
+++ lab5/routes/message.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  //return json with id, name, and message
+  res.json({message: 'Hello World', status: 'ok'});
+
 });
 
 module.exports = router;
