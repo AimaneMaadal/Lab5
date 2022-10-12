90f0ab4dbab7e5700dc9a4de5b3378f29d8f9cc9
AimaneMaadal <aimanemaadal@gmail.com>
2022-10-12 22:42:01 +0200

    feat: router post tested with postmen

--- lab5/routes/message.js
+++ lab5/routes/message.js
@@ -2,11 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-
+router.get('/', function (req, res, next) {
   //return json with id, name, and message
-  res.json({message: 'Hello World', status: 'ok'});
-
+  res.json({
+    message: 'Hello World',
+    status: 'ok',
+  });
 });
+
+router.get('/:id', function (req, res, next) {
+  //return json with id, name, and message
+  let id = req.params.id;
+  res.json({
+    message: `GETTING messages with ID ${id}`,
+    status: 'ok',
+  });
+});
+
+router.post('/', function (req, res, next) {
+  //return json with id, name, and message
+  res.json({
+    message: 'POSTING a new message for user Pikachu',
+    status: 'ok',
+  });
+});
+
+
 
 module.exports = router;
