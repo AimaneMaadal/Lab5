d71c443f4ee958de88e4d94fa88b69293437aaf3
AimaneMaadal <aimanemaadal@gmail.com>
2022-10-13 00:12:05 +0200

    fix: add cors

--- routes/message.js
+++ routes/message.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var cors = require('cors');
+router.use(cors());
+
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   //return json with id, name, and message
